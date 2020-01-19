69b74309f5e98a99a7ee11fdfe41426497a7f788
Francisco Viana <franciscoviana862@gmail.com>
2020-01-19 18:45:19 -0300

    Created: method Home and Scaffold;

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=5c2564c5";
 
 void main() async {
-
-  http.Response response = await http.get(request);
-  print(jsonDecode(response.body)['results']['currencies']['USD']);
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
   ));
-} 
+}
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-        
-//         primarySwatch: Colors.blue,
-//       ),
-//       //home: MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return jsonDecode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
+}
